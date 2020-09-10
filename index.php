bd564980b11598852a2b9d555e0b3f71f16a1798
athifah27zahra <50130026+athifah27zahra@users.noreply.github.com>
2020-09-10 20:27:17 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,10 +6,13 @@
 	<title>T-Shirt Shop</title>
 	<link rel="stylesheet" href="banner.css">
 	<link rel="stylesheet" href="footer.css">
+	<link rel="stylesheet" href="stylebaru.css">
+	<link rel="stylesheet" href="stylesheet.css">
 </head>
 <body>
-	<?php include 'navbar.html'; ?>
+	<?php include 'indexHeader.html'; ?>
 	<?php include 'header.html'; ?>
+	<?php include 'index.html'; ?>
 	<?php include 'footer.html'; ?>
 </body>
 </html>
